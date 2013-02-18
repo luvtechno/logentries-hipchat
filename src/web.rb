577b3d2484aff4081178dbd4be82a58f39740807
Yoshinori Kawasaki <ykawasaki@gmail.com>
2013-02-19 01:37:12 +0900

    Send a test message to HipChat

--- src/web.rb
+++ src/web.rb
@@ -1,7 +1,12 @@
 require 'sinatra'
 
+api_token = ENV['HIPCHAT_TOKEN']
+client = HipChat::Client.new(api_token)
+room_id = ENV['HIPCHAT_ROOM_ID']
+
 get '/' do
   puts "Hello World!"
+  client[room_id].send('Logentries', 'Hello!')
   "Hello World!"
 end
 
