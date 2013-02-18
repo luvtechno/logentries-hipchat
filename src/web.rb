a9fbc294ce013d7def595732e9863e22e928cf16
Yoshinori Kawasaki <ykawasaki@gmail.com>
2013-02-19 01:43:45 +0900

    Send an alert message to hipchat

--- src/web.rb
+++ src/web.rb
@@ -3,14 +3,19 @@ require 'sinatra'
 api_token = ENV['HIPCHAT_TOKEN']
 client = HipChat::Client.new(api_token)
 room_id = ENV['HIPCHAT_ROOM_ID']
+username = 'Logentries'
 
 get '/' do
   puts "Hello World!"
-  client[room_id].send('Logentries', 'Hello!')
+  client[room_id].send(username, 'Hello!')
   "Hello World!"
 end
 
 post '/alert' do
+  puts params
   payload = JSON.parse(params[:payload])
   puts payload
+  message = "%s: %s" % [payload['alert']['name'], payload['event']['m']]
+  puts message
+  client[room_id].send(username, message, color: 'red')
 end
